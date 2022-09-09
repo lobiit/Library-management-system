9e3dd5e767fbfe401d561042c09f44b17ee7bbef
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-09 16:03:57 +0300

    fibonacci series

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-option = input()
-
-while option != None;
+x =int(input())
+a = 0
+b=1
+if x == 0:
+    print(0)
+elif x==1:
+    print(1)
+else:
+    print(a)
+    print(b)
+    for i in range(1, x):
+        c = a + b
+        a = b
+        b = c
+        print(b)
