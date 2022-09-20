a06941ded22af8f11e380206f7f0402c5f4de343
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-20 15:47:22 +0300

    The trivial approach would be to first combine both input arrays and then perform a sort. If we have n = arr1.length + arr2.length, then this gives a time complexity of O(n \cdot \log{}n)O(n⋅logn). This would be a good approach if the input arrays were not sorted, but because they are sorted, we can use the two-pointers technique to improve to O(n)O(n).
    We can build the answer array ans one element at a time. Start two pointers at the first index of each array, and compare their elements. At each iteration, we have 2 values. Whichever value is lower needs to come first in the answer, so add it to the answer and move the respective pointer.

--- check_for_target.py
+++ check_for_target.py
@@ -18,3 +18,31 @@ def check_for_target(arr):
 
 nums = [1, 2, 4, 6, 8, 9, 14, 15]
 print(check_for_target(nums))
+
+
+# Given two sorted integer arrays, return an array that combines both of them and is also sorted.
+def combine(arr1, arr2):
+    ans = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j += 1
+
+    return ans
+
+
+nums2 = [1, 62, 24, 6, 8, 19, 14, 15]
+nums2.sort()
+print(combine(nums, nums2))
