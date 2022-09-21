20d8e7bf3d3d848e8044fee88f1117a9cbf9610a
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-21 10:45:43 +0300

    Two Pointer Approach
    The intuition behind this approach is that the area formed between the lines will always be limited by the height of the shorter line. Further, the farther the lines, the more will be the area obtained.
    
    We take two pointers, one at the beginning and one at the end of the array constituting the length of the lines. Futher, we maintain a variable \text{maxarea}maxarea to store the maximum area obtained till now. At every step, we find out the area formed between them, update \text{maxarea}maxarea and move the pointer pointing to the shorter line towards the other end by one step.

---
+++ sliding window.py
@@ -0,0 +1,24 @@
+def find_length(nums, k):
+    left = curr = ans = 0
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+        ans = max(ans, right - left + 1)
+
+    return ans
+
+
+def find_length(s):
+    left = curr = ans = 0
+    for right in range(len(s)):
+        if s[right] == "0":
+            curr += 1
+        while curr > 1:
+            if s[left] == "0":
+                curr -= 1
+            left += 1
+        ans = max(ans, right - left + 1)
+
+    return ans
